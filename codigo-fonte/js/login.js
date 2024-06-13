833610ba768b2c903fd84971245744b576251769
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-06-13 16:24:35 -0300

    Adicionado a interatividade visual da página login

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -4,32 +4,66 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.querySelector("#loginForm");
     const emailField = document.querySelector("#email");
     const passwordField = document.querySelector("#senha");
+    const showPassword = document.querySelector("#show-password");
 
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
         const email = emailField.value;
         const senha = md5(passwordField.value);
+        let message = "";
 
         if (testLogin(email, senha)) {
             console.log("Login efetuado com sucesso.");
             loginForm.submit();
             window.location.href = "index.html";
-        } else {
-            console.error("E-mail ou senha inválidos. Por favor, tente novamente");
         }
     });
+
+    showPassword.addEventListener("click", function() {
+        if (passwordField.getAttribute("type") === "password") {
+            passwordField.setAttribute("type", "text");
+            passwordField.focus();
+            showPassword.src = "images/eye.svg";
+        } else {
+            passwordField.setAttribute("type", "password");
+            passwordField.focus();
+            showPassword.src = "images/eye-slash.svg";
+        }
+    })
 });
 
 function testLogin(emailRef, passwordRef) {
-    for (let index = 0; index < database.length; index++) {
-        if (emailRef === database[index].email &&
-            passwordRef === database[index].senha) {
-                console.log("Credenciais válidas");
-                return true;
+    if (emailRef !== "" && passwordRef !== "") {
+        for (let index = 0; index < database.length; index++) {
+            if (emailRef === database[index].email &&
+                passwordRef === database[index].senha) {
+                    console.log("Credenciais válidas");
+                    return true;
+            }            
         }
+        message = "E-mail ou senha inválidos. Por favor, tente novamente."
+        errorMsg(message);
+        console.error(message);
+        return false;
         
+    } else {
+        message = "Por favor, insira o seu e-mail e senha.";
+        errorMsg(message);
+        console.error(message);
+        return false;
     }
-    console.error("Credenciais inválidas");
-    return false;
+}
+
+function errorMsg(messageRef) {
+    const passwordDiv = document.querySelector("#password-field");
+    const testErrorP = passwordDiv.querySelector("p#login-error");
+    if (testErrorP == null) {
+        const errorMsg = document.createElement("p");
+        errorMsg.id = "login-error";
+        errorMsg.innerHTML = messageRef;
+        passwordDiv.appendChild(errorMsg);    
+    } else {
+        testErrorP.innerHTML = messageRef;
+    }
 }
