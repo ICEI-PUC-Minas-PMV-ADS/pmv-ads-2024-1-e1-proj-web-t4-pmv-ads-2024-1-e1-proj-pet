8e853c34f75b0237a3f10dfe22573506294652d0
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-06-13 11:24:33 -0300

    Atualizando o teste e validação de email e senha em login.js

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -1,58 +1,35 @@
+const database = JSON.parse(localStorage.getItem("database"));
+
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("loginForm");
+    const loginForm = document.querySelector("#loginForm");
+    const emailField = document.querySelector("#email");
+    const passwordField = document.querySelector("#senha");
 
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        const email = document.getElementById("email").value;
-        const senha = document.getElementById("senha").value;
+        const email = emailField.value;
+        const senha = md5(passwordField.value);
 
-        const cadastroJSON = localStorage.getItem("cadastros");
-
-        if (cadastroJSON) {
-            const cadastros = JSON.parse(cadastroJSON);
-            const senhaMD5 = md5(senha);
-
-            const usuario = cadastros.find(cadastros => cadastros.email === email && cadastros.senha === senhaMD5);
-
-            if (usuario) {
-                alert("Login realizado com sucesso!");
-                window.location.href = "index.html";
-            } else {
-                alert("E-mail ou senha incorretos. Por favor, tente novamente.");
-            }
-
+        if (testLogin(email, senha)) {
+            console.log("Login efetuado com sucesso.");
+            loginForm.submit();
+            window.location.href = "index.html";
         } else {
-            alert("Nenhum cadastro encontrado. Por favor, faça o cadastro primeiro.");
+            console.error("E-mail ou senha inválidos. Por favor, tente novamente");
         }
     });
 });
-document.addEventListener("DOMContentLoaded", function () {
-    const form_recuperar = document.getElementById("form_recuperar");
 
-    form_recuperar.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const email = document.getElementById("Entrar").value;
-
-
-        const cadastroJSON = localStorage.getItem("cadastros");
-
-        if (cadastroJSON) {
-            const cadastros = JSON.parse(cadastroJSON);
-            const email = (email);
-
-            const usuario = cadastros.find(cadastros => cadastros.email === email);
-
-            if (usuario) {
-                alert("Login realizado com sucesso!");
-                window.location.href = "index.html";
-            } else {
-                alert("E-mail  incorreto, Por favor, tente novamente.");
-            }
-
-        } else {
-            alert("Nenhum cadastro encontrado. Por favor, faça o cadastro primeiro.");
+function testLogin(emailRef, passwordRef) {
+    for (let index = 0; index < database.length; index++) {
+        if (emailRef === database[index].email &&
+            passwordRef === database[index].senha) {
+                console.log("Credenciais válidas");
+                return true;
         }
-    });
-});
+        
+    }
+    console.error("Credenciais inválidas");
+    return false;
+}
