a6adace6480c66c2fed21138ce6ce9774af7938e
Ana Cavalcanti <anacavancalti@gmail.com>
2024-06-17 14:35:40 +0200

    Homepage fixed
    A homepage estava em loop infinito por causa do comando location.reload() Porém, o local storage para de receber os dados sem esse comando. Resolução ainda a ser buscada

--- codigo-fonte/js/dbload.js
+++ codigo-fonte/js/dbload.js
@@ -1,46 +1,49 @@
 const databaseTest = JSON.parse(localStorage.getItem("database"));
 const authenticationTest = JSON.parse(localStorage.getItem("authentication"));
-const authenticatedSessionTest = JSON.parse(sessionStorage.getItem("authSession"));
+const authenticatedSessionTest = JSON.parse(
+  sessionStorage.getItem("authSession")
+);
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (databaseTest === null) {
-        fetchData();
-        location.reload();
-    }
-    
-    if (authenticationTest === null) {
-        const authentication = JSON.parse(localStorage.getItem("authentication")) || [];
-        const database = JSON.parse(localStorage.getItem("database"));
-        for (let index = 0; index < database.length; index++) {
-            const authenticationHash = {
-                id: index + 1,
-                hash: md5(database[index].id + database[index].email + database[index].senhaHash)
-            }
-            authentication.push(authenticationHash);
-        }
-        localStorage.setItem("authentication", JSON.stringify(authentication));
-    }
-    
-    if (authenticatedSessionTest === null) {
-        const authSession = JSON.parse(sessionStorage.getItem("authSession")) || [];
-        sessionStorage.setItem("authSession", JSON.stringify(authSession));
-    }
+  if (databaseTest === null) {
+    fetchData();
+  }
 
+  if (authenticationTest === null) {
+    const authentication =
+      JSON.parse(localStorage.getItem("authentication")) || [];
+    const database = JSON.parse(localStorage.getItem("database"));
+    for (let index = 0; index < database.length; index++) {
+      const authenticationHash = {
+        id: index + 1,
+        hash: md5(
+          database[index].id + database[index].email + database[index].senhaHash
+        ),
+      };
+      authentication.push(authenticationHash);
+    }
+    localStorage.setItem("authentication", JSON.stringify(authentication));
+  }
+
+  if (authenticatedSessionTest === null) {
+    const authSession = JSON.parse(sessionStorage.getItem("authSession")) || [];
+    sessionStorage.setItem("authSession", JSON.stringify(authSession));
+  }
 });
 
 async function fetchData() {
-    try {
-        const response = await fetch("db/database.json");
-        if(!response.ok) {
-            throw new Error ("Could not fetch resource.")
-        }
-        data = await response.json();
-        const database = JSON.parse(localStorage.getItem("database")) || [];
-        for (let index = 0; index < data.length; index++) {
-            database.push(data[index]);     
-        }
-        localStorage.setItem("database", JSON.stringify(database));
-    } catch (error) {
-        console.error(error);
+  try {
+    const response = await fetch("db/database.json");
+    if (!response.ok) {
+      throw new Error("Could not fetch resource.");
     }
+    data = await response.json();
+    const database = JSON.parse(localStorage.getItem("database")) || [];
+    for (let index = 0; index < data.length; index++) {
+      database.push(data[index]);
+    }
+    localStorage.setItem("database", JSON.stringify(database));
+  } catch (error) {
+    console.error(error);
+  }
 }
