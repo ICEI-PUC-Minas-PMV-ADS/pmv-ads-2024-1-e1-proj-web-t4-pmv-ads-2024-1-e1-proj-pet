5a7be77c0ca759f2325745f0cf45a77fb936a700
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-06-22 01:52:07 -0300

    Update index.js
    - Contabiliza somente a quantidade de pet sitters cadastrados;
    - Não mostra a parte de cadastro no conteúdo principal quando está logado;

--- codigo-fonte/js/index.js
+++ codigo-fonte/js/index.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const registerContainer = document.querySelector(".users");
+    const searchContainer = document.querySelector(".sitters");
 
-    // Fazer uma lógica para pegar quem está com serPetSitter === true;
     const numRegister = document.querySelector("#num-register");
-    numRegister.innerHTML = database.length;
+    numRegister.innerHTML = numbersPetSitters();
+
+    if (authenticatedSession !== null && authenticatedSession.length > 0) {
+        registerContainer.setAttribute("style", "display: none");
+        searchContainer.setAttribute("style", "margin: auto");
+    } else {
+        registerContainer.setAttribute("style", "display: block");
+    }
 });
+
+function numbersPetSitters() {
+    let quantity = 0;
+    for (let index = 0; index < database.length; index++) {
+        if (database[index].serPetSitter) {
+            quantity++;
+        }
+    }
+    return quantity;
+}
